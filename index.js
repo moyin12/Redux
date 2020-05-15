36350b8daa61c08f0bf2a62b5daae2a62ace6a75
Moyinoluwa Boluwatife Ariyo <moyin.ariyo@gmail.com>
2020-05-15 23:05:34 +0100

    Changed the action.type string to variables

--- index.js
+++ index.js
@@ -28,13 +28,19 @@ const createStore = (reducer) => {
     }
 }
 // App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 const todos = (state = [], action) => {
     switch(action.type) {
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo])
-        case 'REMOVE_TODO' :
+        case REMOVE_TODO :
             return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO' :
+        case TOGGLE_TODO :
             return state.map((todo) => todo.item !== action.id ? todo
             : Object.assign({}, todo, {complete: !todo.complete }))
         default :
@@ -44,9 +50,9 @@ const todos = (state = [], action) => {
 
 const goals = (state = [], action) => {
     switch(action.type) {
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal])
-        case 'REMOVE_GOAL' :
+        case REMOVE_GOAL :
             return state.filter((goal) => goal.id !== action.id)
         default :
             return state
@@ -66,7 +72,7 @@ store.subscribe(() => {
 })
 
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
        id: 0,
        name: 'Learn Redux',
