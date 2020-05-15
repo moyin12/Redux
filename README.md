39e8a67acb97297ae52348be3d9e3fc1d806d704
Moyinoluwa Boluwatife Ariyo <moyin.ariyo@gmail.com>
2020-05-15 18:52:23 +0100

    Listening to the state changes

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ The store consists of the following.
 2. a way to get the state tree.
 3. a way to listen and respond to the state changing.
 4. a way to update the state.
+
+## First File is the index.js file
+1. Created the store.
+2. Created the state variable. local(private)
+3. Created the get state function to access the state variable.(it returns an object)
+4. Created a Listeners object
--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const createStore = () => {
 
     // The state
     let state
+    let listeners = []
 
     // Get the state
     const getState = () => state
+    
+    // Listening to the state changes
+    const subscibe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
 
     return {
         getState
