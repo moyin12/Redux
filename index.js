ba19a196161c651ac386032b0e86442ef36956cd
Moyinoluwa Boluwatife Ariyo <moyin.ariyo@gmail.com>
2020-05-15 22:23:40 +0100

    Added new states

--- index.js
+++ index.js
@@ -29,10 +29,17 @@ const createStore = (reducer) => {
 }
 // App code
 const todos = (state = [], action) => {
-    if (action.type === 'ADD_TODO'){
-        return state.concat([action.todo])
+    switch(action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case 'REMOVE_TODO' :
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+            return state.map((todo) => todo.item !== action.id ? todo
+            : Object.assign({}, todo, {complete: !todo.complete }))
+        default :
+            return state
     }
-    return state
 }
 
 
